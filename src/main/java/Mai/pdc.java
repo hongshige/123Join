a485557ef390a7f5336ad2f713ca9be3c4f69fad
hongshige <2402210783@qq.com>
2021-01-14 20:10:22 +0800

    1

---
+++ src/main/java/Mai/pdc.java
@@ -0,0 +1,17 @@
+package Mai;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class pdc extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    System.out.println("[123警告]插件已启动");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
